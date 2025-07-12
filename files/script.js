a43986c7a40d5e728334cff8ae87f7f11138d966
AmirBehin <amirmdbn1383@outlook.com>
2025-07-12 08:11:33 +0330

    First commit

---
+++ files/script.js
@@ -0,0 +1,13 @@
+﻿function menubazbaste() {
+
+            var menubar = document.getElementById("menubar");
+            if (rightarrow.style.display == "none") {
+                rightarrow.style.display = "inherit";
+                menubar.style.display = "none";
+                menu.style.display = "block";
+            } else {
+                rightarrow.style.display = "none";
+                menubar.style.display = "inherit";
+                menu.style.display = "none";
+            }
+        }
